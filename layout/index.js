9a3d21bb2bdbc961b54a9ad9b525571c7fc2d7a2
nguyentiennghiep <nguyen_tien_nghiep@mediado.jp>
2018-12-04 17:34:24 +0900

    first commit

---
+++ layout/index.js
@@ -0,0 +1,27 @@
+var dropDown = document.getElementById("drop-link");
+var collapseButton = document.getElementById("collapse-button");
+var navBar = document.getElementById("nav-bar");
+var dropEnable = false;
+var collapseEnable = false;
+function dropMenu() {
+  if (dropEnable === false) {
+    dropDown.classList.add("drop-active");
+    dropEnable = true;
+  } else {
+    dropDown.classList.remove("drop-active");
+    dropEnable = false;
+  }
+}
+
+
+function onCollapse() {
+  if (collapseEnable === false) {
+    collapseButton.classList.add("open");
+    navBar.classList.add("open-menu");
+    collapseEnable = true;
+  } else {
+    collapseButton.classList.remove("open");
+    navBar.classList.remove("open-menu");
+    collapseEnable = false;
+  }
+}
